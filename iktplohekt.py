3eec20227506b40b21541d925d0ddc49f23f912f
Axy the Foxolotl <89694715+zozozoli2007@users.noreply.github.com>
2024-02-22 12:09:46 +0100

    Add files via upload

---
+++ iktplohekt.py
@@ -0,0 +1,20 @@
+'''def rendszam_e(rendsz):
+    itelet = 0
+    return itelet
+
+rsze = input('Rendszám: ')
+
+a = rendszam_e(rsze)
+
+if (a == 0):
+    print('Nem rendszám!')
+elif (a == 1):
+    print('Régi rendszám.')
+else:
+    print('Új rendszám.')'''
+
+
+li = []
+li.append(rsze)
+if len(li,4) == '-':
+    
