4cc27fd4c54b5cbd6b2b89189210c3f7cd996b7d
Axy the Foxolotl <89694715+zozozoli2007@users.noreply.github.com>
2024-03-06 19:15:19 +0100

    Update

--- iktplohekt.py
+++ iktplohekt.py
@@ -4,41 +4,42 @@ li = list(rsze)
 betu=("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
 szam=("0","1","2","3","4","5","6","7","8","9")
 
-if li[5] == '-':
-    if li[2] == "-":
+def rendszam(rsze):
+    if li[5] == '-':
+        if li[2] == "-":
+            if li[0].upper() in betu:
+                print("Jo")
+            if li[1].upper() in betu:
+                print("jo")
+            if li[3].upper() in betu:
+                print("jo")
+            if li[4].upper() in betu:
+                print("jo")
+            if li[6].upper() in szam:
+                print("jo")
+            if li[7].upper() in szam:
+                print("jo")
+            if li[8].upper() in szam:
+                print("jo")
+            else:
+                print("nem jo")
+    elif li[4]=="-":
+        print("Műkszik")
         if li[0].upper() in betu:
-            print("Jo")
+            print("jo")
         if li[1].upper() in betu:
+            print("jo")
+        if li[2].upper() in betu:
             print("jo")
         if li[3].upper() in betu:
             print("jo")
-        if li[4].upper() in betu:
+        if li[5].upper() in szam:
             print("jo")
         if li[6].upper() in szam:
             print("jo")
         if li[7].upper() in szam:
             print("jo")
-        if li[8].upper() in szam:
-            print("jo")
         else:
             print("nem jo")
-elif li[4]=="-":
-    print("Műkszik")
-    if li[0].upper() in betu:
-        print("jo")
-    if li[1].upper() in betu:
-        print("jo")
-    if li[2].upper() in betu:
-        print("jo")
-    if li[3].upper() in betu:
-        print("jo")
-    if li[5].upper() in szam:
-        print("jo")
-    if li[6].upper() in szam:
-        print("jo")
-    if li[7].upper() in szam:
-        print("jo")
     else:
         print("nem jo")
-else:
-    print("nem jo")
