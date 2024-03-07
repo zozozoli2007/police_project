3f7d379e64e90a3813bcde31b6386625fb0adf2b
Axy the Foxolotl <89694715+zozozoli2007@users.noreply.github.com>
2024-03-07 18:17:08 +0100

    Update

--- rendszam_axy2.py
+++ rendszam_axy2.py
@@ -1,7 +1,13 @@
 #Alprogram
 
 def rendszam_e(rsze):
-    
+    betu=("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    szam=("0","1","2","3","4","5","6","7","8","9")
+    if rsze[3]=="-":
+        if rsze[:3] in betu and rsze[4:] in szam:
+            return 1
+    else:
+        return 0
     return
 
 #Főprogram
